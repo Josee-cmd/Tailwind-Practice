9e1ad36465cb139fbb5dbba81ffa4ce9160c18ce
Josee-cmd <josesslcedo@gmail.com>
2024-01-03 17:59:09 -0500

    add photos

--- src/Components/AppComponents.jsx
+++ src/Components/AppComponents.jsx
@@ -3,7 +3,7 @@ import Form from './Form';
 /*-----------------------------------*/
 function AppComponents() {
   return (
-    <div className='w-5/6 border flex flex-col items-center my-5 rounded-2xl lg:w-4/5 lg:flex-row lg:justify-start h-5/6'>
+    <div className='w-5/6 border lg:border-none flex flex-col items-center my-5 rounded-2xl lg:w-4/5 lg:flex-row lg:justify-start h-5/6'>
         <AppImg/>
         <Form/>
     </div>
