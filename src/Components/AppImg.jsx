5cdc22cafdba18a2ecf54965689d35ebf0c3f163
Josee-cmd <josesslcedo@gmail.com>
2024-01-10 10:16:24 -0500

    Changes logo

--- src/Components/AppImg.jsx
+++ src/Components/AppImg.jsx
@@ -1,15 +1,16 @@
-import img from "../assets/facebook.webp";
+// import img from "../assets/facebook.webp";
+import Josan from "../assets/Josan.png"
 function AppImg() {
   return (
     <div className="w-60 lg:w-1/2 mx-5 lg:mb-28">
       <h1 className="text-6xl font-bold text-blue-500 hidden lg:block my-5">
-        facebook
+        Josan
       </h1>
       <p className="text-3xl hidden lg:block ">
-        Facebook te ayuda a comunicarte y compartir con las personas que forman
+        Josan te ayuda a comunicarte y compartir con las personas que forman
         parte de tu vida.
       </p>
-      <img src={img} alt="Not found" className="lg:hidden" />
+      <img src={Josan} alt="Not found" className="lg:hidden" />
     </div>
   );
 }
