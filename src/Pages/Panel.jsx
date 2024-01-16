676c7a6f2a054f7b56a6185a74d1d3f3c5ca9511
Josee-cmd <josesslcedo@gmail.com>
2024-01-15 20:51:16 -0500

    add in home page styles of lyrics

--- src/Pages/Panel.jsx
+++ src/Pages/Panel.jsx
@@ -9,14 +9,14 @@ import {DateComponent} from '../Components/Date'
 function Panel() {
   return (
     <div className="h-full grid grid-cols-2 gap-x-4 gap-y-1 pb-16 mx-3">
-      <div className="text-4xl flex justify-start items-center pl-2 my-5"><p>Hey, <b>Jose</b></p></div>
+      <div className="text-4xl flex justify-start items-center pl-2 my-5 "><p>Hey, <b>Jose</b></p></div>
       <div className="flex justify-end items-center">
         <div className="w-12 h-12 border border-gray-300 rounded-full flex justify-center items-center">
           <button className="cursor-pointer"><img src={plus} alt="no found" className="w-12"/></button>
         </div>
       </div>
-      <div className="col-span-2 border border-black h-24 rounded-2xl text-xl flex justify-start pl-3 items-center my-2 font-bold"><DateComponent/></div>
-      <div className="col-span-2 w-full text-xl border border-black h-24 rounded-2xl flex justify-start items-center pl-3 font-bold">
+      <div className="col-span-2 border border-black h-24 rounded-2xl text-xl flex justify-start pl-3 items-center my-2 font-medium"><DateComponent/></div>
+      <div className="col-span-2 w-full text-xl border border-black h-24 rounded-2xl flex justify-start items-center pl-3 font-medium">
         <h1>La proxima alarma sonara en: 8-HR</h1>
       </div>
       <Card src={notification} content="Ver o añadir una alarma" />
